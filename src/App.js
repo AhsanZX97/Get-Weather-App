f0226fe2b62ab515de8a630730d12825d5c9a938
Ahsan Zia <ahsan97@hotmail.co.uk>
2019-10-21 20:27:58 +0100

    form gives back json data of a city

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "1df3b360b5967d372e9c55e9abff8abf"
+
 class App extends React.Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    // api call
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render() {
     return(
       <div>
         <Titles></Titles>
-        <Form></Form>
+        <Form getWeather={this.getWeather}></Form>
         <Weather/>
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,11 @@ class Form extends Component {
     render() {
         return (
             <div>
-                
+                <form onSubmit={this.props.getWeather}>
+                    <input type="text" name="city" placeholder="City..."/>
+                    <input type="text" name="country" placeholder="Country..."/>
+                    <button>Get Weather</button>
+                </form>
             </div>
         )
     }
