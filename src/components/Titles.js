553581968e968976b7f457acd62ef75d999e309d
Ahsan Zia <ahsan97@hotmail.co.uk>
2019-10-24 20:40:36 +0100

    changed react components from class to const

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 
-class Titles extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out the temperature of your city</p>
-            </div>
-        )
-    }
-}
+const Titles = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find out the temperature of your city</p>
+    </div>
+)
+
 
 export default Titles;
